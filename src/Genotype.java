e0b653cbc7996f27f717f2714327a4f81f029545
Unknown <226014@student.pwr.edu.pl>
2018-01-14 00:42:07 +0100

    added ox crossover and little mutation refactor

--- src/Genotype.java
+++ src/Genotype.java
@@ -15,11 +15,15 @@ public class Genotype {
 
     public Genotype crossover(Genotype parent1, Genotype parent2, int problemSize){
         Genotype child = new Genotype(problemSize);
-        List sameGens = new ArrayList();
+        List parent1Gens = new ArrayList();
+        List parent2Gens = new ArrayList();
         Random rand = new Random();
 
         int sectionStartPosition = rand.nextInt(problemSize);
+        System.out.println("start = " + sectionStartPosition);
         int sectionEndPosition = rand.nextInt(problemSize);
+        System.out.println("end = " + sectionEndPosition);
+
         if (sectionStartPosition > sectionEndPosition){
             int buf = sectionStartPosition;
             sectionStartPosition = sectionEndPosition;
@@ -31,24 +35,63 @@ public class Genotype {
             child.currentRoute[sectionStartPosition + i] = parent1.currentRoute[sectionStartPosition + i];
         }
 
-        for (int i = 0; i < (sectionEndPosition - sectionStartPosition); i++){
-            sameGens.add(parent1.currentRoute[sectionStartPosition + i]);
+        System.out.println("Child: ");
+        for (int i = 0; i < problemSize; i++){
+            System.out.print(child.currentRoute[i]);
         }
 
-        //TODO OX crossover
-        for (int i = sectionEndPosition; i < problemSize; i++){
+        for (int i = 0; i < (sectionEndPosition - sectionStartPosition); i++){
+            parent1Gens.add(parent1.currentRoute[sectionStartPosition + i]);
+        }
 
+        System.out.print("Parent1Gens: " + parent1Gens.toString());
+
+        //OX crossover
+        for (int i = sectionEndPosition; i < problemSize; i++){
+            if (!parent1Gens.contains(parent2.currentRoute[i])){
+                parent2Gens.add(parent2.currentRoute[i]);
+            }
+        }
+
+        for (int i = 0; i < sectionEndPosition; i++){
+            if (!parent1Gens.contains(parent2.currentRoute[i])){
+                parent2Gens.add(parent2.currentRoute[i]);
+            }
+        }
+
+        System.out.print("Parent2Gens: " + parent2Gens.toString());
+
+        int j = 0;
+        for (int i = sectionEndPosition; i < problemSize; i++, j++){
+            child.currentRoute[i] = (int)parent2Gens.get(j);
+        }
+
+        for (int i = 0; i < sectionStartPosition; i++, j++) {
+            child.currentRoute[i] = (int)parent2Gens.get(j);
+        }
+
+        System.out.println("Child: ");
+        for (int i = 0; i < problemSize; i++){
+            System.out.print(child.currentRoute[i]);
         }
 
         return child;
     }
 
-    public void mutation(int problemSize){
+    public void mutation(){
         Random rand = new Random();
-        int mutIndex1 = rand.nextInt(problemSize);
-        int mutIndex2 = rand.nextInt(problemSize);
+        int mutIndex1 = rand.nextInt(this.currentRoute.length);
+        int mutIndex2 = rand.nextInt(this.currentRoute.length);
+        if (mutIndex1 == mutIndex2){
+            mutIndex2 = rand.nextInt(this.currentRoute.length);
+        }
         int buf = this.currentRoute[mutIndex1];
         this.currentRoute[mutIndex1] = this.currentRoute[mutIndex2];
         this.currentRoute[mutIndex2] = buf;
+
+        System.out.println("After mutation Child: ");
+        for (int i = 0; i < problemSize; i++){
+            System.out.print(this.currentRoute[i]);
+        }
     }
 }
